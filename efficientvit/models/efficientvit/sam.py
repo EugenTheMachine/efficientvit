4880062240d4ee7efdd35ffbd54cb26e6f369064
eugen <lazoskal24@gmail.com>
2025-05-14 19:32:05 +0300

    -

--- efficientvit/models/efficientvit/sam.py
+++ efficientvit/models/efficientvit/sam.py
@@ -70,7 +70,7 @@ class SamResize:
         self.size = size
 
     def __call__(self, image: np.ndarray) -> np.ndarray:
-        print(image.shape)
+        # print(image.shape)
         h, w = image.shape[-2:]
         long_side = max(h, w)
         if long_side != self.size:
